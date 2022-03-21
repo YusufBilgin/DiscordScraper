8097f9a5785ada33fe7edd73a6ed70f9046c90ff
Yusuf Bilgin <ykbilgin61@gmail.com>
2022-03-21 05:31:06 +0300

    refactor: implemented match case statment instead if else

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os
-from tabulate import tabulate
 from dotenv import load_dotenv
 from colorama import Fore, init
 
 from modules.menu import menu
 from modules.utils import wait
-from modules.cli_forms import choose_action, save_or_not
-from modules.file_operations import save_content_to_txt
+from modules.cli_forms import choose_action
 from modules.get_requests import Requests
 from modules.operations import *
 
@@ -22,45 +20,31 @@ def main():
     while True:  
         user_input = input("Please choose one of the actions given above: ")
 
-        if user_input == 1 or user_input == "1":
-            selected_action = choose_action()
+        match user_input:
+            case "1":
+                selected_action = choose_action()
+                match selected_action:
+                    case 0:
+                        print_user_account_data(req)
+                    case 1:
+                        print_user_friends(req)
+                    case 2:
+                        print_user_dm_channels(req)
+                    case 3:
+                        print_user_guilds(req)
+                    case 4:
+                        channel_id = input("channel id: ")
+                        print_specific_channel_messages(channel_id, req)
+                    case _:
+                        pass
 
-            match selected_action:
-                case 1:
-                    pass
-                case 2:
-                    pass
-                case 3:
-                    pass
-                case 4:
-                    pass
-                case _:
-                    pass
-
-            if selected_action == 0:
-                print_user_account_data(req)
-                
-            elif selected_action == 1:
-                print_user_friends(req)
-
-            elif selected_action == 2:
-                print_user_dm_channels(req)
-
-            elif selected_action == 3:
-                print_user_guilds(req)
-
-            elif selected_action == 4:
-                channel_id = input("channel id: ")
-                print_specific_channel_messages(channel_id, req)
+                print("Press any key to continue")
+                wait()
+            case "2":
+                print("info text")
+            case _:
+                print("Please choose a valid operation")
             
-            print("Press any key to continue")
-            wait()
-
-        elif user_input == 2 or user_input == "2":
-            print("info text")
-        else:
-            print("Please choose a valid operation")
-
 
 if __name__ == "__main__":
     try:
