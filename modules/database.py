0a931bca686fa984e15ec88312e7c2881ea9ffd6
Yusuf Bilgin <ykbilgin61@gmail.com>
2022-03-23 22:08:26 +0300

    feat: add, create table on db func to Database class

--- modules/database.py
+++ modules/database.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 
+
 class Database:
     def __init__(self) -> None:
         self.create_connection()
@@ -20,3 +21,24 @@ class Database:
             print(f"The error {Error} occurred")
 
         return None
+
+    def create_table(self, table_name: str) -> None:
+        """Create a table on db with the given name 
+
+        Args:
+            table_name (str): table name
+
+        Returns:
+            None: none
+        """
+
+        query = F"CREATE TABLE IF NOT EXISTS {table_name} "
+
+        try:    
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Error:
+            print(f"The error {Error} occurred")
+
+        return None
+
