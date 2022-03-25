7bbbcec3dafea7b8f24a893f43552c81dde65703
Yusuf Bilgin <ykbilgin61@gmail.com>
2022-03-25 04:11:17 +0300

    feat: add new option to select list

--- modules/cli_forms.py
+++ modules/cli_forms.py
@@ -7,9 +7,10 @@ def choose_action() -> int:
         'Print my account infos',
         'Print my friends list',
         'Print my dm channels',
-        'Print my servers',
+        'Print my guilds',
         'Print messages from a spesific channel',
-        'Print messages from a dm channel'
+        'Print messages from a dm channel',
+        'Print guild channels',
     ]
     option, index = pick(options, title)
 
